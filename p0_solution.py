3bcd628e0a2ea41967a3501d4f42c11ef7c82219
Steven Krukowski <spk170@stanford.edu>
2017-09-21 01:43:53 -0700

    Moved the Connection class to it's only file
    Added logging capabilities for auto flight (need to make this available for manually flying).

--- p0_solution.py
+++ p0_solution.py
@@ -1,98 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import threading
-import time
-import sys
+
 import numpy as np
-from pymavlink import mavutil
+
 import utm
+import time
+import logger
+import connection
 import os
+from pymavlink import mavutil
 
-# force use of mavlink v2.0
-os.environ['MAVLINK20'] = '1'
-
-class Connection:
-    def __init__(self, device, fn):
-        self.callback = fn
-        # TODO: mavutil connection here...
-        self.master = mavutil.mavlink_connection(device, source_system=190)
-        self._running = True
-        self.read_handle = threading.Thread(
-            target=self.read_thread)  # TODO: start read thread here
-        self.read_handle.start()
-        self.write_handle = 0  # TODO: decide if want a write thread, and if so, start it here
-
-        self.target_system = 0
-        self.target_component = 0
-
-
-    def read_thread(self):
-
-        while self._running:
-
-            # get the next message
-            # NOTE: this is a blocking call, which is why we have a thread for it
-            msg = self.master.recv_match(blocking=True)
-
-            # if it's a good message, send it back to the callback
-            if msg.get_type() != 'BAD_DATA':
-                target_system = msg.get_srcSystem()
-                target_component = msg.get_srcComponent()
-                self.callback(msg.get_type(), msg)
-
-            # want to send a heartbeat periodically, so can just do that when we receive one
-            if msg.get_type() == 'HEARTBEAT':
-                # print("sending heartbeat")
-                # send -> type, autopilot, base mode, custom mode, system status
-                self.master.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_GCS,
-                                               mavutil.mavlink.MAV_AUTOPILOT_INVALID,
-                                               0, 0, mavutil.mavlink.MAV_STATE_ACTIVE)
-
-        print("read ended")
-
-    def send_message(self, msg):
-        a = 0
-        # TODO: send the message
-
-    def send_waypoint_command(self, hold_time, acceptance_radius, fly_through,
-                              yaw, x, y, z):
-        self.master.mav.command_int_send(
-            self.target_system, self.target_component,
-            mavutil.mavlink.MAV_FRAME_LOCAL_NED, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 1, 0,
-            hold_time, acceptance_radius, fly_through, yaw, x, y, z)
-
-    def send_takeoff_command(self, z):
-        self.master.mav.command_int_send(
-            self.target_system, self.target_component,
-            mavutil.mavlink.MAV_FRAME_LOCAL_NED, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF_LOCAL, 1,
-            0, 0, 0, 0, 0, x, y, z)
-
-    def send_local_position_command(self, x, y, z):
-        self.master.mav.command_int_send(
-            self.target_system, self.target_component,
-            mavutil.mavlink.MAV_FRAME_LOCAL_NED, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF_LOCAL, 1,
-            0, 0, 0, 0, 0, x, y, z)
-
-    def send_mav_command(self, command_type, param1, param2, param3, param4, x,
-                         y, z):
-        self.master.mav.command_int_send(
-            self.target_system, self.target_component,
-            mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, command_type, 1, 0, param1,
-            param2, param3, param4, x, y, z)
-
-    def arm_drone(self):
-        self.master.arducopter_arm()
-
-    def disarm_drone(self):
-        self.master.arducopter_disarm()
-
-    def disconnect(self):
-        # stop running the read loop, and wait for the thread to finish
-        self._running = False
-        self.read_handle.join()
-
-        # close the mavutil connection (tcp/udp/serial, etc)
-        self.master.close()
 
 
 class Drone:
@@ -106,11 +23,14 @@ class Drone:
         self.heading = None
         self.mode = None
         self.connected = False
+        
+        
+        self.log = logger.Logger(os.path.join("Logs","navLog.txt"))
 		
 		
     #This method will be provided to the students
     def connect(self, device):
-        self.connection = Connection(device, self.decode_mav_msg)
+        self.connection = connection.Connection(device, self.decode_mav_msg)
         while self.connected != True:
             print("Waiting to Connect")
             time.sleep(1)
@@ -118,6 +38,7 @@ class Drone:
     #This method will be provided to the students
     def disconnect(self):
         self.connection.disconnect()
+        self.log.close()
 
     #TODO: Fill in this method
     #This method should set the vehicle mode to GUIDED, arm the motors and return True when the motors are successfully armed
@@ -192,8 +113,8 @@ class Drone:
         #Monitor both the position and the velocity, terminate when the vehicle is near the ground with a near-zero vertical velocity
         while self.global_position[2] - land_pos[2] > 0.1:
             if self.global_position[2] - self.global_home[2] < 0.1:
-			    if abs(self.global_velocity[2]) < 0.1:
-					return True
+                if abs(self.global_velocity[2]) < 0.1:
+                    return True
             print(self.global_position[2]-land_pos[2])
             time.sleep(0.1)
         print("Finished Landing")
@@ -230,13 +151,24 @@ class Drone:
             # print(self.motors_armed)
             # TODO: correctly parse the state information
         elif name is 'GLOBAL_POSITION_INT':
+            
             self.global_position[0] = float(msg.lon) / (10 ** 7)
+            data = [self.global_position[0]]
             self.global_position[1] = float(msg.lat) / (10 ** 7)
+            data.append(self.global_position[1])
             self.global_position[2] = float(msg.relative_alt) / 1000
+            data.append(self.global_position[2])
             self.global_velocity[0] = float(msg.vx) / 100
+            data.append(self.global_velocity[0])
             self.global_velocity[1] = float(msg.vy) / 100
+            data.append(self.global_velocity[1])
             self.global_velocity[2] = -float(msg.vz) / 100
+            data.append(self.global_velocity[2])
             self.heading = float(msg.hdg) / 100
+            data.append(self.heading)
+            
+            self.log.log_data(data)
+            
         # TODO: add other messages of interest....
         else:
             print(name)
@@ -245,16 +177,18 @@ class Drone:
 #TODO: Fill in this function
 #This function should command the vehicle to arm, takeoff to a 3m altitude, fly a 10m box at 3m altitude, land and disarm 
 def takeoff_and_fly_box(drone):
+    altitude = 5.0
+    box_size = 10.0
     print('Arming Vehicle')
     drone.arm_vehicle()
     time.sleep(1)
     print('Launching Vehicle')
-    drone.takeoff(3.0)
+    drone.takeoff(altitude)
     time.sleep(1)    
 
     #Box waypoints specified in global frame
     global box_waypoints
-    box_waypoints = calculate_box(drone.global_home)
+    box_waypoints = calculate_box(drone.global_home,box_size,altitude)
     print("Done calculating box")
 
     for i in range(len(box_waypoints)):
@@ -273,11 +207,11 @@ def takeoff_and_fly_box(drone):
 
 
 #This function will not be provided, it is part of my solution
-def calculate_box(global_home):
+def calculate_box(global_home,box_size,altitude):
     
     global_waypoints = np.zeros((4, 3))
-    local_waypoints = np.array([[10.0, 0.0, -3.0],[10.0, 10.0, -3.0],[0.0, 10.0, -3.0],[0.0, 0.0, -3.0]])
-    for i in xrange(0,4):
+    local_waypoints = np.array([[box_size, 0.0, -altitude],[box_size, box_size, -altitude],[0.0, box_size, -altitude],[0.0, 0.0, -altitude]])
+    for i in range(0,4):
         global_waypoints[i, :] = local_to_global(local_waypoints[i, :], global_home)
 
     return global_waypoints
