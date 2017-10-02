752bfe5acd42c05f0515158073431d0962de3ee9
Steven Krukowski <spk170@stanford.edu>
2017-10-01 17:55:06 -0700

    Changed naming convention Separated callbacks for different Mavlink messages

--- connection.py
+++ connection.py
@@ -6,8 +6,7 @@ import os
 os.environ['MAVLINK20'] = '1'
 
 class Connection:
-    def __init__(self, device, fn):
-        self.callback = fn
+    def __init__(self, device):
         self.master = mavutil.mavlink_connection(device, source_system=190)
         self._running = True
         #self.read_handle = threading.Thread(
@@ -17,7 +16,10 @@ class Connection:
 
         self.target_system = 0
         self.target_component = 0
-
+        self.callbacks = {}
+    
+    def set_callback(self,msg,fn):
+        self.callbacks[msg] = fn
 
     def wait_for_message(self):
         msg = self.master.recv_match(blocking=True,timeout=10)
@@ -25,7 +27,8 @@ class Connection:
             return True
         else:
             if(msg.get_type() != 'BAD_DATA'):
-                self.callback(msg.get_type(),msg)
+                if msg.get_type() in self.callbacks.keys():
+                    self.callbacks[msg.get_type()](msg)
         
             if msg.get_type() == 'HEARTBEAT':
                 # send -> type, autopilot, base mode, custom mode, system status
@@ -33,36 +36,14 @@ class Connection:
                                            mavutil.mavlink.MAV_AUTOPILOT_INVALID,
                                            0, 0, mavutil.mavlink.MAV_STATE_ACTIVE)
             return False
-    def read_thread(self):
-
-        while self._running:
-
-            # get the next message
-            # NOTE: this is a blocking call, which is why we have a thread for it
-            msg = self.master.recv_match(blocking=True)
-            
-
-            # if it's a good message, send it back to the callback
-            if msg.get_type() != 'BAD_DATA':
-                self.callback(msg.get_type(), msg)
-
-            # want to send a heartbeat periodically, so can just do that when we receive one
-            if msg.get_type() == 'HEARTBEAT':
-                # print("sending heartbeat")
-                # send -> type, autopilot, base mode, custom mode, system status
-                self.master.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_GCS,
-                                               mavutil.mavlink.MAV_AUTOPILOT_INVALID,
-                                               0, 0, mavutil.mavlink.MAV_STATE_ACTIVE)
-
-        print("read ended")
 
 
     def send_mav_command(self, command_type, param1, param2, param3, param4, x,
                          y, z):
         self.master.mav.command_int_send(
             self.target_system, self.target_component,
-            mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, command_type, 1, 0, param1,
-            param2, param3, param4, int(x), int(y), z)
+            mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, command_type, 1, 0, float(param1),
+            float(param2), float(param3), float(param4), int(x), int(y), float(z))
 
     def disconnect(self):
         # stop running the read loop, and wait for the thread to finish
--- drone.py
+++ drone.py
@@ -23,7 +23,6 @@ def calculate_box(global_home):
     local_waypoints = np.array([[10.0, 0.0, -3.0],[10.0, 10.0, -3.0],[0.0, 10.0, -3.0],[0.0, 0.0, -3.0]])
     for i in range(0,4):
         global_waypoints.extend([frame_utils.local_to_global(local_waypoints[i, :], global_home)])
-
     return global_waypoints
 
 class Drone:
@@ -53,8 +52,11 @@ class Drone:
 		
     #Provided
     def connect(self, device):
-        self.connection = connection.Connection(device, self.decode_mav_msg)
-        self.connection.wait_for_message()
+        self.connection = connection.Connection(device)
+        timeout = self.connection.wait_for_message()
+        
+        if ~timeout:
+            self.connected = True
         
     
     #Provided
@@ -64,125 +66,155 @@ class Drone:
         
     #Provided
     def manual(self):
-        self.in_mission
+        self.in_mission = False
         self.connection.send_mav_command(mavutil.mavlink.MAV_CMD_NAV_GUIDED_ENABLE, 0, 0,
                              0, 0, 0, 0, 0)
         self.state = States.MANUAL
+        print('Commanded Manual')
         return
     
     #Initiate the ARMING state, put the vehicle in guided mode and arm
     def arm(self):
-        #TODO: fill out this method
+        self.state = States.ARMING
+        self.connection.send_mav_command(mavutil.mavlink.MAV_CMD_NAV_GUIDED_ENABLE, 1, 0,
+                                     0, 0, 0, 0, 0)
+        self.connection.send_mav_command(mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 1,
+                                     0, 0, 0, 0, 0, 0)          
         return
     
     #Initiate the TAKEOFF state, command the vehicle the target altitude (m)
     def takeoff(self,altitude=3.0):
-        #TODO: fill out this method
+        self.state = States.TAKEOFF
+        self.target_position = np.copy(self.global_position)
+        self.target_position[2] = altitude
+        self.connection.send_mav_command(mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0,
+                                     0, 0, altitude)
         return
     
     #Initiate the WAYPOINT state, command the vehicle to the position specified as Lat, Long, Alt
     def waypoint(self,target):
-        #TODO: fill out this method
+        self.state = States.WAYPOINT
+        self.connection.send_mav_command(mavutil.mavlink.MAV_CMD_NAV_LOITER_UNLIM, 0, 0, 0, 0, target[1]*10**7, target[0]*10**7, target[2])
         return
     
     #Initiate the LANDING state, command the vehicle to specified altitude (m)
     def land(self, altitude=0.0):
-        #TODO: fill out this method
+        self.state = States.LANDING
+        self.target_position = np.copy(self.global_home)
+        self.target_position[2] = altitude
+        self.connection.send_mav_command(mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0,
+                                     0, 0, self.target_position[2])
+
         return
     
     #Initiate the DIARMING state, command the vehicle to disarm
     def disarm(self):
-        #TODO; fill out this method
+        self.state = States.DISARMING
+        self.connection.send_mav_command(mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0,
+                                     0, 0, 0, 0, 0, 0)
         return
      
     #Provided
-    def manual_callback(self):
-        
-        return True
+    def manual_transition(self):
+        #Do nothing
+        return
     
     
     #Save the current position as the home position and transition to the next state when the vehicle is armed and in guided mode
-    def arming_callback(self):
-        #TODO: fill out this callback
+    def arming_transition(self):
+        if self.motors_armed & self.guided:
+            self.global_home = np.copy(self.global_position)
+            self.takeoff()
         return
         
     
     #Transition to the next state when the target altitude is reach
-    def takeoff_callback(self):
-        # TODO: fill out this callback
+    def takeoff_transition(self):
+        if self.global_position[2] >= 0.95*self.target_position[2]:
+            self.all_waypoints = calculate_box(self.global_home)
+            self.target_position = self.all_waypoints.pop(0)
+            self.waypoint(self.target_position)
         return
     
         
     # Transition to the next state when the target waypoint is reached (within 1m)
-    def waypoint_callback(self):
-        #TODO: fill out this callback
+    def waypoint_transition(self):
+        local_target = frame_utils.global_to_local(self.target_position,self.global_home)
+        local_position = frame_utils.global_to_local(self.global_position,self.global_home)
+        if np.linalg.norm(local_target[0:2]-local_position[0:2])<1.0:
+            if len(self.all_waypoints)>0:
+                self.target_position = self.all_waypoints.pop(0)
+                self.waypoint(self.target_position)
+            else:
+                self.land(self.global_home[2]-1.0)
+                
         return                
        
 
     # Transition to the next state when the drone is on the ground
-    def landing_callback(self):
-        #TODO: fill out this callback
+    def landing_transition(self):
+        if self.global_position[2] - self.global_home[2] < 0.05:
+            if self.global_velocity[2] < 0.05:
+                self.disarm()
         return
     
     # Transition to the next state when the drone is disarmed
-    def disarming_callback(self):
-        #TODO: fill in this callback
+    def disarming_transition(self):
+        if ~self.motors_armed:
+            self.manual()
         return
 
-    #This method is provided
-    def decode_mav_msg(self, name, msg):
-        print('Data Received')
-        # NOTE: this effectively becomes a callback of the main connection thread
-        #This will be implemented for the students and sort the mavlink message to different callbacks for different types
-        #It may actually just populate the vehicle class data directly
-        self.connected = True        
-        if name is 'STATUSTEXT':
-            name #Do nothing
 
-        elif name is 'HEARTBEAT':
-            #print('Heartbeat Message')
-            self.motors_armed = (msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED) != 0
-            self.guided = (msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_GUIDED_ENABLED) != 0
-            # Correctly parse the state information
-        elif name is 'GLOBAL_POSITION_INT':
-            
-            self.global_position[0] = float(msg.lon) / (10 ** 7)
-            data = [self.global_position[0]]
-            self.global_position[1] = float(msg.lat) / (10 ** 7)
-            data.append(self.global_position[1])
-            self.global_position[2] = float(msg.relative_alt) / 1000
-            data.append(self.global_position[2])
-            self.global_velocity[0] = float(msg.vx) / 100
-            data.append(self.global_velocity[0])
-            self.global_velocity[1] = float(msg.vy) / 100
-            data.append(self.global_velocity[1])
-            self.global_velocity[2] = -float(msg.vz) / 100
-            data.append(self.global_velocity[2])
-            self.heading = float(msg.hdg) / 100
-            data.append(self.heading)
-            
-            for i in range(3):
-                data.append(self.target_position[i])
-            
-            self.log.log_data(data)
-            
-        else:
-            print(name)
+    def heartbeat_callback(self,msg):
+        self.connected = True
+        self.motors_armed = (msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED) != 0
+        self.guided = (msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_GUIDED_ENABLED) != 0
+        
+        if self.state in self.check_state.keys():
+            self.check_state[self.state]()
+        
+    def global_position_callback(self, msg):
+        self.connected = True
+        self.global_position[0] = float(msg.lon) / (10 ** 7)
+        data = [self.global_position[0]]
+        self.global_position[1] = float(msg.lat) / (10 ** 7)
+        data.append(self.global_position[1])
+        self.global_position[2] = float(msg.relative_alt) / 1000
+        data.append(self.global_position[2])
+        self.global_velocity[0] = float(msg.vx) / 100
+        data.append(self.global_velocity[0])
+        self.global_velocity[1] = float(msg.vy) / 100
+        data.append(self.global_velocity[1])
+        self.global_velocity[2] = -float(msg.vz) / 100
+        data.append(self.global_velocity[2])
+        self.heading = float(msg.hdg) / 100
+        data.append(self.heading)
+        
+        for i in range(3):
+            data.append(self.target_position[i])
+        
+        self.log.log_data(data)
+        
+        if self.state in self.check_state.keys():
+            self.check_state[self.state]()
+
     
     #Provided, sets the list of callbacks      
-    def set_callbacks(self):
-        self.check_state[States.MANUAL] = self.manual_callback
-        self.check_state[States.ARMING] = self.arming_callback
-        self.check_state[States.TAKEOFF] = self.takeoff_callback
-        self.check_state[States.WAYPOINT] = self.waypoint_callback
-        self.check_state[States.LANDING] = self.landing_callback
-        self.check_state[States.DISARMING] = self.disarming_callback
+    def set_transitions(self):
+        self.check_state[States.MANUAL] = self.manual_transition
+        self.check_state[States.ARMING] = self.arming_transition
+        self.check_state[States.TAKEOFF] = self.takeoff_transition
+        self.check_state[States.WAYPOINT] = self.waypoint_transition
+        self.check_state[States.LANDING] = self.landing_transition
+        self.check_state[States.DISARMING] = self.disarming_transition
 
     #Provided
     def init_mission(self):
         self.connect("tcp:127.0.0.1:5760")
         self.in_mission = True
-        self.set_callbacks()
+        self.set_transitions()
+        self.connection.set_callback('HEARTBEAT',self.heartbeat_callback)
+        self.connection.set_callback('GLOBAL_POSITION_INT',self.global_position_callback)
 
     #Provided, runs the mission
     def run_mission(self):
@@ -195,7 +227,6 @@ class Drone:
             if timeout:
                 print('Connection Timeout')
                 self.connected = False
-            self.check_state[self.state]() #Check for a state transition
         
         if self.connected:
             self.connection.disconnect()
@@ -204,6 +235,7 @@ class Drone:
     #Provided
     def init_manual(self):
         self.connect("tcp:127.0.0.1:5760")
+        self.connection.set_callback('GLOBAL_POSITION_INT',self.global_position_callback)
     
     #Provided, runs the logger while flying manually
     def fly_manual(self):
