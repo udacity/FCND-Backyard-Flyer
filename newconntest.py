5ac52f2b41dadd4d369ff01930f265488c948449
Adrien Perkins <adrienp@stanford.edu>
2017-10-17 18:16:00 -0700

    create example test class to check parsing of messages with jmavsim

--- newconntest.py
+++ newconntest.py
@@ -1,16 +1,23 @@
 from connection import mavlink_connection
 
-#device = "" #"tcp:127.0.0.1:5760"
-device = "udp:127.0.0.1:14540"
-mavconn = mavlink_connection.MavlinkConnection(device, threaded=False)
+
+class TestClass:
+
+	def __init__(self):
+		#device = "" #"tcp:127.0.0.1:5760"
+		device = "udp:127.0.0.1:14540"
+		self.mavconn = mavlink_connection.MavlinkConnection(device, threaded=False)
 
 
-@mavconn.on_message('*')
-def testing(_, name, msg):
-    print("got a message")
-    print(name)
-    print(msg)
+		@self.mavconn.on_message('*')
+		def testing(_, name, msg):
+		    print("got a message")
+		    print(name)
+		    print(msg)
 
+	def start(self):
+		self.mavconn.testcallback()
+		self.mavconn.start()
 
-mavconn.testcallback()
-mavconn.start()
+test = TestClass()
+test.start()
